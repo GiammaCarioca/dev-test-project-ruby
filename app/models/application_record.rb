08fc9503a355ba872985adb34e1af6849ae2737c
Thiago Giammattey <giammattey@gmail.com>
2022-02-01 07:27:20 -0300

    refactoring

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,25 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.upload(file)
+    CSV.foreach(file.path, col_sep: "\t", headers: true, header_converters: :symbol, encoding: 'UTF-8') do |row|
+      item = assign_from_row(row).save
+
+      report(item)
+    end
+  end
+
+  def self.report(item)
+    counter = 0
+
+      if item
+        counter += 1
+      else
+        puts self.errors.full_messages.join(',').to_s
+      end
+
+      puts "Imported #{counter} #{self.to_s}."
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,23 +6,8 @@ class Customer < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
-  def self.upload(file)
-    counter = 0
-
-    CSV.foreach(file.path, col_sep: "\t", headers: true, header_converters: :symbol, encoding: 'UTF-8') do |row|
-      customer = Customer.assign_from_row(row)
-
-      if customer.save
-        counter += 1
-      else
-        puts customer.errors.full_messages.join(',').to_s
-      end
-
-      puts "Imported #{counter} customers."
-    end
-  end
-
   def self.assign_from_row(row)
     Customer.where(name: row[:purchaser_name]).first_or_initialize
   end
+  
 end
